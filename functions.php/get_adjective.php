84c1aa95a784695a7af8665554d5a0ce299d389e
Ben Cerjan <bcerjan@gmail.com>
2022-12-29 13:36:10 -0600

    Added license text.

--- functions.php/get_adjective.php
+++ functions.php/get_adjective.php
@@ -1,4 +1,22 @@
 <?php
+/**
+ *    Copyright (c) 2020 Ben Cerjan, Lief Esbenshade
+ *
+ *    This file is part of smbc_stocks.
+ *
+ *    smbc_stocks is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Affero General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    smbc_stocks is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with smbc_stocks.  If not, see <https://www.gnu.org/licenses/>.
+**/
 
 /**
  * Function to get a random adjective from our list
