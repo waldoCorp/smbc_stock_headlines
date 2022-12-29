e6f769c8b036afda60aaf1b486df4ca4bbcf90c9
Ben Cerjan <bcerjan@gmail.com>
2022-12-29 13:37:22 -0600

    Added license text

--- website/index.php
+++ website/index.php
@@ -1,5 +1,22 @@
 <!DOCTYPE html>
-
+<!--
+ -    Copyright (c) 2020 Ben Cerjan, Lief Esbenshade
+ -
+ -    This file is part of smbc_stocks.
+ -
+ -    smbc_stocks is free software: you can redistribute it and/or modify
+ -    it under the terms of the GNU Affero General Public License as published by
+ -    the Free Software Foundation, either version 3 of the License, or
+ -    (at your option) any later version.
+ -
+ -    smbc_stocks is distributed in the hope that it will be useful,
+ -    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ -    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ -    GNU Affero General Public License for more details.
+ -
+ -    You should have received a copy of the GNU Affero General Public License
+ -    along with smbc_stocks.  If not, see <https://www.gnu.org/licenses/>.
+-->
 <html lang="en">
 <head>
 <meta charset="utf=8">
